eac49f41b4f2c0956a286749426e6e33ff79265a
velomad <48247308+velomad@users.noreply.github.com>
2020-10-08 15:41:48 +0530

    quick react app test

--- Scrappers/myntra/scraper.js
+++ Scrappers/myntra/scraper.js
@@ -9,6 +9,7 @@ module.exports.scraper = async (url, callBack) => {
 	});
 	const page = await browser.newPage();
 
+	
 	await page.setUserAgent(
 		"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
 	);
