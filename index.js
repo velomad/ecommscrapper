81ed4255b8d6ec266719e3790ad994678bc55b78
velomad <48247308+velomad@users.noreply.github.com>
2020-10-08 14:34:27 +0530

    quick react app test

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+const path = require("path")
 const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
 require("./cronjobs/flipkart");
