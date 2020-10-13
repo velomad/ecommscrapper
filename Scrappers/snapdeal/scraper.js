6c6ab67469672fd15711fdf54eb8eb5d0b056a7c
A00079 <bajay0007@gmail.com>
2020-10-13 18:13:04 +0530

    Fixes

--- Scrappers/snapdeal/scraper.js
+++ Scrappers/snapdeal/scraper.js
@@ -4,8 +4,12 @@ var catgories = require('./categories.js');
 
 module.exports.scraper = async (url, callBack) => {
 	const browser = await puppeteer.launch({ 
-		args: [`--proxy-server=http=194.67.37.90:3128`],
-		headless: false 
+		args: [
+		`--proxy-server=http=194.67.37.90:3128`,
+		'--no-sandbox',
+		'--disable-setuid-sandbox'
+		],
+		headless: true 
 	});
 	const page = await browser.newPage();
 	await page.setUserAgent(userAgent.toString());
